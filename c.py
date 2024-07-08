d43108b384c76b35b4bc70d8b72275362b850dbc
TheresaHHH <44091390+TheresaHHH@users.noreply.github.com>
2024-07-08 19:01:39 -0400

    Update c.py

--- c.py
+++ c.py
@@ -5,8 +5,8 @@ from  openpyxl.utils.dataframe import dataframe_to_rows
 
 # Load the workbook
 fileName='Result.xlsx'
-sheetName='RotatedResult'
 wb = load_workbook(fileName)
+sheetName=wb.sheetnames[0]
 
 # Select the Result sheet
 sheet = wb[sheetName]
@@ -20,15 +20,17 @@ for j in range(0, 24, 6): #swith these two lines.  col by col/row by row
 #print(tables1[0])  
 #print(tables1[1])          
 
-
 #####export data into a txt file#######
 
 with open('result.txt', 'w') as f:
     # Write the header line
-    f.write('Block, row, column, data\n')
+    f.write(f"{'Block':<5} {'row':<5} {'column':<7} {'data'}\n")
     
     for i in range(1,49):
         for row in range(6):
             for col in range(6):
                 data=tables1[i-1][row][col]
-                f.write(f'{i}, {row+1}, {col+1}, {data}\n')
+                #f.write(f'{i}, {row+1}, {col+1}, {data}\n')
+                f.write(f"{i:<5} {row + 1:<5} {col + 1:<7} {data}\n")
+                
+print("result.txt is generated")
