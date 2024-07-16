d3083a7706c0da3055231c6732df5a090b044ea6
SharonHg <44092503+SharonHg@users.noreply.github.com>
2024-07-16 11:54:18 -0400

    Update a.py

--- a.py
+++ a.py
@@ -25,5 +25,34 @@ excelSheets = excel_File.sheet_names;
 #How many columns and rows in each small table?
 firstTableColumn = 12;
 firstTableRow = 4;
-
 firstTables = [];
+
+#Read the excel file and create tables
+for sheets in range(len(excelSheets)):
+    #Dataframe
+    file = excel_File.parse(excelSheets[sheets], header=None, names=None, index_col=None, usecols=None) ;
+
+    ##How many tables are on the left side?
+    for j in range(4):
+        firstTable=[];
+        for i in range(firstTableRow):
+            tempRow = file.loc[i+j*firstTableRow, 0 : firstTableColumn-1].values.tolist();
+            firstTable.extend(tempRow);
+        firstTables.insert(len(firstTables), firstTable);
+
+    ##How many tables are on the right side?
+    for j in range(7):
+        firstTable=[];
+        for i in range(firstTableRow):
+            tempRow = file.loc[i+j*firstTableRow, firstTableColumn: firstTableColumn*2-1].values.tolist();
+            firstTable.extend(tempRow);
+        firstTables.insert(len(firstTables), firstTable);        
+    print('sheet: '+ str(sheets) + ' is read.');
+
+'''
+#testing    
+print('firstTables')
+for i in range(len(firstTables)):
+    print(str(i))
+    print(firstTables[i])
+'''
